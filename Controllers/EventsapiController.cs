97295be7e58cd5cfafa98fc73b7215b451cc5221
Brayden Traas <btraas@gmail.com>
2017-12-02 01:45:53 -0800

    API services init

---
+++ Controllers/EventsapiController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZenithSocietyA2.Data;
+using ZenithSocietyA2.Models;
+using ZenithSocietyA2.Data;
+
+namespace ZenithSocietyA2.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Events")]
+    public class EventsapiController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EventsapiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Studentsapi
+        [HttpGet]
+        public IEnumerable<Event> GetEvents()
+        {
+            return _context.Events;
+        }
+
+        // GET: api/Eventsapi/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEvent([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var @event = await _context.Events.SingleOrDefaultAsync(m => m.EventId == id);
+
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(@event);
+        }
+
+        // PUT: api/Studentsapi/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEvent([FromRoute] int id, [FromBody] Event @event)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != @event.EventId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(@event).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Studentsapi
+        [HttpPost]
+        public async Task<IActionResult> PostEvent([FromBody] Event @event)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Events.Add(@event);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEvent", new { id = @event.EventId }, @event);
+        }
+
+        // DELETE: api/Studentsapi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStudent([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var @event = await _context.Events.SingleOrDefaultAsync(m => m.EventId == id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            _context.Events.Remove(@event);
+            await _context.SaveChangesAsync();
+
+            return Ok(@event);
+        }
+
+        private bool StudentExists(int id)
+        {
+            return _context.Events.Any(e => e.EventId == id);
+        }
+    }
+}
